0520abdc8a10c404d8b05a7f3cfb4b52f7494b59
IrishBruse <Econn50@outlook.com>
2023-03-18 20:16:18 +0000

    Add Grayscale Rendering

--- CelticCode/Application.cs
+++ CelticCode/Application.cs
@@ -25,29 +25,59 @@ public class Application : IDisposable
     private Pipeline pipeline;
     private Texture fontAtlasTexture;
     private TextureView surfaceTextureView;
+    private float lineHeight = 14;
     private readonly IWindow window;
 
     private float scroll;
-    private GeometryBuffer textBuffer = new();
+    private GeometryBuffer textBuffer;
 
     private ResourceLayout textureLayout;
     private ResourceSet textureSet;
     private Matrix4x4 mvp;
+    private float ScrollOffset => scroll / window.Size.Y * 14 * 2;
+
+    private static readonly Matrix4x4 ViewMatrix = Matrix4x4.CreateLookAt(Vector3.UnitZ, Vector3.Zero, Vector3.UnitY);
 
     public Application(IWindow window)
     {
         this.window = window;
+        GenFile();
+    }
+
+    private void GenFile()
+    {
+        using StreamWriter writer = File.CreateText(@"A:\CelticCode\test.txt");
+
+        int offset = 0;
+
+        for (int j = 0; j < 1000; j++)
+        {
+            for (int i = 0; i < 95 * 3; i++)
+            {
+                char letter = (char)(((i + offset) % 95) + 33);
+                writer.Write(letter);
+            }
+            writer.Write(' ');
+            writer.Write('\n');
+
+            offset++;
+        }
     }
 
     public void Load()
     {
-        string file = File.ReadAllText(@"A:\CelticCode\test.txt");
+        using StreamReader reader = File.OpenText(@"A:\CelticCode\test.txt");
 
         GraphicsDevice = CreateGraphicsDevice();
         factory = GraphicsDevice.ResourceFactory;
         commandList = factory.CreateCommandList();
 
-        GenerateFontGeometry(file);
+
+        FontGenerator.GenerateGrayscaleTexture(GraphicsDevice, ref fontAtlasTexture);
+
+        textBuffer = new(GraphicsDevice);
+
+        GenerateFontGeometry(reader);
 
         ShaderDescription vertexShaderDesc = new(ShaderStages.Vertex, File.ReadAllBytes("Assets/Shaders/base.vert"), "main");
         ShaderDescription fragmentShaderDesc = new(ShaderStages.Fragment, File.ReadAllBytes("Assets/Shaders/base.frag"), "main");
@@ -63,8 +93,8 @@ public class Application : IDisposable
         VertexLayoutDescription vertexLayout = new(
             new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2),
             new VertexElementDescription("TexCoords", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2),
-            new VertexElementDescription("Foreground", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float4),
-            new VertexElementDescription("Background", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float4)
+            new VertexElementDescription("Foreground", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
+            new VertexElementDescription("Background", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3)
         );
 
         pipelineDescription.ShaderSet = new ShaderSetDescription(new VertexLayoutDescription[] { vertexLayout }, shaders);
@@ -96,27 +126,21 @@ public class Application : IDisposable
             GraphicsDevice.PointSampler
         ));
 
-        textBuffer.Upload(GraphicsDevice);
-
         Resize(window.Size);
     }
 
-    private void GenerateFontGeometry(string file)
+    private void GenerateFontGeometry(StreamReader reader)
     {
-        FontGenerator.Generate(GraphicsDevice, ref fontAtlasTexture);
         uint w = fontAtlasTexture.Width;
         uint h = fontAtlasTexture.Height;
-
-        // Atlas
-        // textBuffer.AddQuad(new(0, 0), new(w, h), new(0, 0), new(1, 1));
 
         float penX = 0;
         float penY = 0;
 
-        float lineHeight = 14;
-
-        foreach (char letter in file)
+        while (!reader.EndOfStream)
         {
+            char letter = (char)reader.Read();
+
             if (letter == '\r')
             {
                 continue;
@@ -151,12 +175,27 @@ public class Application : IDisposable
         IKeyboard keyboard = input.Keyboards[0];
         IMouse mouse = input.Mice[0];
 
-        float lineheight = 14;
-        int lines = 3;
+        mouse.Cursor.StandardCursor = StandardCursor.IBeam;
+
+        keyboard.KeyDown += (k, key, i) =>
+        {
+            switch (key)
+            {
+                case Key.PageDown:
+                scroll += 45;
+                break;
+
+                case Key.PageUp:
+                scroll -= 45;
+                break;
+            }
+        };
 
         mouse.Scroll += (s, e) =>
         {
-            scroll -= s.ScrollWheels[0].Y / window.Size.Y * 2f * lineheight * lines;
+            const int lines = 3;
+
+            scroll -= s.ScrollWheels[0].Y * lines;
 
             if (scroll < 0)
             {
@@ -167,14 +206,18 @@ public class Application : IDisposable
 
     public void Update(double dt)
     {
-        window.Title = $"CelticCode";
+        _ = dt;
+
+        window.Title = $"CelticCode - {scroll}";
     }
 
     public void Draw(double dt)
     {
         _ = dt;
 
-        Matrix4x4 mat = mvp * Matrix4x4.CreateTranslation(0, scroll, 0);
+        textBuffer.Upload();
+
+        Matrix4x4 mat = mvp * Matrix4x4.CreateTranslation(0, ScrollOffset, 0);
 
         commandList.UpdateBuffer(matrixBuffer, 0, ref mat);
 
@@ -199,8 +242,7 @@ public class Application : IDisposable
     {
         // 0,0 in the top left
         Matrix4x4 projMat = Matrix4x4.CreateOrthographicOffCenter(0f, size.X, size.Y, 0f, -1f, 1f);
-        Matrix4x4 viewMat = Matrix4x4.CreateLookAt(Vector3.UnitZ, Vector3.Zero, Vector3.UnitY);
-        mvp = viewMat * projMat;
+        mvp = ViewMatrix * projMat;
 
         commandList.UpdateBuffer(matrixBuffer, 0, ref mvp);
 
--- CelticCode/Freetype/FontGenerator.cs
+++ CelticCode/Freetype/FontGenerator.cs
@@ -14,7 +14,7 @@ public class FontGenerator
 {
     public static Dictionary<char, Glyph> Glyphs { get; set; } = new();
 
-    public static unsafe void Generate(GraphicsDevice graphicsDevice, ref Texture texture)
+    public static unsafe void GenerateSubpixelTexture(GraphicsDevice graphicsDevice, ref Texture texture)
     {
         using FreeTypeLibrary lib = new();
 
@@ -23,8 +23,6 @@ public class FontGenerator
         FreeTypeFaceFacade ft = new(lib, face);
 
         FT_Set_Pixel_Sizes(ft.Face, 0, 12);
-
-        FT_Library_SetLcdFilterWeights(lib.Native, new byte[] { 8, 77, 86, 77, 8 });
 
         uint atlasWidth = 0;
         uint atlasHeight = 0;
@@ -89,6 +87,77 @@ public class FontGenerator
             graphicsDevice.UpdateTexture(texture, img, xoffset, 0, 0, w, h, 1, 0, 0);
 
             xoffset += ft.GlyphBitmap.width / 3;
+        }
+    }
+
+    public static unsafe void GenerateGrayscaleTexture(GraphicsDevice graphicsDevice, ref Texture texture)
+    {
+        using FreeTypeLibrary lib = new();
+
+        FT_Err(FT_New_Face(lib.Native, "Assets/Fonts/CascadiaCode.ttf", 0, out nint face));
+
+        FreeTypeFaceFacade ft = new(lib, face);
+
+        FT_Set_Pixel_Sizes(ft.Face, 0, 12);
+
+        uint atlasWidth = 0;
+        uint atlasHeight = 0;
+
+        for (uint index = 32; index < 128; index++)
+        {
+            FT_Err(FT_Load_Char(ft.Face, index, FT_LOAD_TARGET_LCD));
+            atlasWidth += ft.GlyphBitmap.width;
+            atlasHeight = Math.Max(atlasHeight, ft.GlyphBitmap.rows);
+        }
+
+        texture = graphicsDevice.ResourceFactory.CreateTexture(TextureDescription.Texture2D(
+            atlasWidth,
+            atlasHeight,
+            1, 1,
+            PixelFormat.R8_G8_B8_A8_UNorm,
+            TextureUsage.Sampled
+        ));
+
+        uint xoffset = 0;
+
+        // Render each glyph to the atlas texture
+        for (int index = 32; index < 128; index++)
+        {
+            char letter = (char)index;
+
+            FT_Err(FT_Load_Char(ft.Face, letter, FT_LOAD_TARGET_NORMAL));
+            FT_Err(FT_Render_Glyph((nint)ft.GlyphSlot, FT_Render_Mode.FT_RENDER_MODE_NORMAL));
+
+            uint w = ft.GlyphBitmap.width;
+            uint h = ft.GlyphBitmap.rows;
+
+            byte[] img = new byte[w * h * 4];
+
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    byte* gray = (byte*)(ft.GlyphBitmap.buffer + (y * ft.GlyphBitmap.pitch) + x + 0);
+
+                    img[((x + (y * w)) * 4) + 0] = *gray;
+                    img[((x + (y * w)) * 4) + 1] = *gray;
+                    img[((x + (y * w)) * 4) + 2] = *gray;
+                    img[((x + (y * w)) * 4) + 3] = 255;
+                }
+            }
+
+            Glyph value = new(
+                new(ft.GlyphMetricHorizontalAdvance, ft.GlyphMetricVerticalAdvance),
+                new(ft.GlyphBitmapLeft, ft.GlyphBitmapTop),
+                new(w, h),
+                xoffset / (float)atlasWidth
+            );
+
+            Glyphs.Add((char)index, value);
+
+            graphicsDevice.UpdateTexture(texture, img, xoffset, 0, 0, w, h, 1, 0, 0);
+
+            xoffset += ft.GlyphBitmap.width;
         }
     }
 
